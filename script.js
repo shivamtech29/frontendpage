49dfd2b4d29a5d0a237a9c30eaf9cf9d0ad0026c
shivamtech29 <shivamagarwal36029@gmail.com>
2020-07-20 00:05:18 +0530

    V1.0

---
+++ script.js
@@ -0,0 +1,24 @@
+function fun()
+    { document.getElementById("demo").innerHTML="welcome";
+}
+function show()
+{
+    document.getElementById("show").style.display="block";
+    
+}
+function hide()
+{
+    document.getElementById("show").style.display="none";
+}
+function btn()
+{
+    document.getElementById("menu").style.display="block";
+}
+function tbtn()
+{
+    document.getElementById("topb").style.display="block";
+}
+function hideb()
+{
+    document.getElementById("menu").style.display="none";
+}
